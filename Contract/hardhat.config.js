06ec2d8ce243be53f7d8517e73e18524baadffc4
Max <max@bvm.network>
2024-12-04 10:43:56 +0700

    update chain

--- Contract/hardhat.config.js
+++ Contract/hardhat.config.js
@@ -39,9 +39,14 @@ module.exports = {
       accounts: [`${process.env.PRIVATE_KEY}`],
       chainId: 137,
     },
-    // hardhat: {
-    //   allowUnlimitedContractSize: true,
-    // },
+    testChain: {
+      url: 'http://127.0.0.1:8545/',
+      accounts: [`0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80`],
+      chainId: 31337,
+    },
+    hardhat: {
+      allowUnlimitedContractSize: true,
+    },
   },
   paths: {
     sources: './contracts',
