ce6b1b7ab6f2ea0901f6468c472f2c9ca9c33655
genesis0000 <genesis0000@bvm.network>
2024-12-10 14:55:46 +0700

    - update

--- contract/contracts/CharacterInfo.sol
+++ contract/contracts/CharacterInfo.sol
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.9;
 
+/*
 import '@openzeppelin/contracts/token/ERC721/ERC721.sol';
 import '@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol';
 import '@openzeppelin/contracts/access/Ownable.sol';
@@ -327,4 +328,4 @@ contract CharacterInfo is ERC721, ERC721URIStorage, Ownable, ERC721Burnable {
             )
         );
     }
-}
+}*/
