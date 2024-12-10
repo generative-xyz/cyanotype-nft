28c8d477a180e6924c86e288a773848bfc237597
genesis0000 <genesis0000@bvm.network>
2024-12-10 12:10:47 +0700

    - update

---
+++ contract/migrations/deploy.js
@@ -0,0 +1,19 @@
+const hre = require('hardhat');
+const Config = require('../index');
+
+async function main() {
+    await Config.initConfig();
+    const [deployer] = await hre.ethers.getSigners();
+    console.log('Deploying contracts with the account:', deployer.address);
+    const mainContract = await hre.ethers.deployContract('CharacterInfo');
+    await mainContract.waitForDeployment();
+    let contractAddress = await mainContract.getAddress();
+    console.log('contract address:', contractAddress);
+    await Config.updateConfig(contractAddress);
+    console.log('Deploy succesful');
+}
+
+main().catch(error => {
+    console.error(error);
+    process.exitCode = 1;
+});
