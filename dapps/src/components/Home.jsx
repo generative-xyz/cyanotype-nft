b44f5702c5834e305345269fed1e9e511f54eaab
genesis0000 <genesis0000@bvm.network>
2024-12-10 12:00:13 +0700

    - update

--- dapps/src/components/Home.jsx
+++ dapps/src/components/Home.jsx
@@ -1,24 +1,24 @@
 import {useEffect, useState} from 'react';
-import ABI from '../../../contract/artifacts/contracts/NFTs.sol/CharacterInfo.json';
+import ABI from '../../../contract/artifacts/contracts/CharacterInfo.sol/CharacterInfo.json';
 import Web3 from 'web3';
 import {Button, Card, Col, Row, Space, Typography} from 'antd';
 import {DATA_INPUT} from "./data";
 import {contractAddress, PRIVATE_KEY} from "../../constant/config";
 
-const { Meta } = Card;
-const { Title } = Typography;
+const {Meta} = Card;
+const {Title} = Typography;
 
 
 function Home() {
-  const [loadings, setLoadings] = useState(false);
-  const [loadingArt, setLoadingArt] = useState(false);
-  const [walletBalance, setWalletBalance] = useState('');
-  const [acc, setAcc] = useState('');
-  const [tokenIdCurrent, setTokenIdCurrent] = useState(0);
-  const [dataJsonArray, setDataJsonArray] = useState([]);
+    const [loadings, setLoadings] = useState(false);
+    const [loadingArt, setLoadingArt] = useState(false);
+    const [walletBalance, setWalletBalance] = useState('');
+    const [acc, setAcc] = useState('');
+    const [tokenIdCurrent, setTokenIdCurrent] = useState(0);
+    const [dataJsonArray, setDataJsonArray] = useState([]);
 
-  var web3 = new Web3(window.ethereum);
-  var contractABI = new web3.eth.Contract(ABI.abi, contractAddress);
+    var web3 = new Web3(window.ethereum);
+    var contractABI = new web3.eth.Contract(ABI.abi, contractAddress);
 
     useEffect(() => {
         web3.eth.accounts.wallet.add(PRIVATE_KEY)
@@ -26,91 +26,90 @@ function Home() {
         setAcc(account)
     }, []);
 
-  const mint = async () => {
-    await contractABI.methods
-        .mint(acc)
-        .send({
-          from: acc,
-        })
-        .then(() => {
-          contractABI
-              .getPastEvents(
-                  'TokenMinted',
-                  {
-                    // fromBlock: 0,
-                    toBlock: 'latest',
-                  },
-                  function (error, events) {
-                    console.log(events);
-                  }
-              )
-              .then(async function (events) {
-                let tokenID = events[0].returnValues.tokenId;
-                const tokenIdConvert = Number(tokenID);
-                setTokenIdCurrent(tokenIdConvert);
-                setLoadings(false);
-              });
-        })
-        .catch(err => {
-          console.error(err.message);
-        });
-  };
+    const mint = async () => {
+        await contractABI.methods
+            .mint(acc)
+            .send({
+                from: acc,
+            })
+            .then(() => {
+                contractABI
+                    .getPastEvents(
+                        'TokenMinted',
+                        {
+                            // fromBlock: 0,
+                            toBlock: 'latest',
+                        },
+                        function (error, events) {
+                            console.log(events);
+                        }
+                    )
+                    .then(async function (events) {
+                        let tokenID = events[0].returnValues.tokenId;
+                        const tokenIdConvert = Number(tokenID);
+                        setTokenIdCurrent(tokenIdConvert);
+                        setLoadings(false);
+                    });
+            })
+            .catch(err => {
+                console.error(err.message);
+            });
+    };
 
 
+    const showArt = async (tokenIdCurrent) => {
+        await contractABI.methods
+            .tokenURI(tokenIdCurrent)
+            .call()
+            .then(result => {
+                console.log('result', result)
+                var cutString = result.substring(29);
+                console.log(cutString)
+                console.log(JSON.parse(atob(cutString)));
+                setDataJsonArray([...dataJsonArray, JSON.parse(atob(cutString))]);
+            })
+            .catch(err => console.log(err));
+        setLoadingArt(false);
 
-  const showArt = async (tokenIdCurrent) => {
-       await contractABI.methods
-          .tokenURI(tokenIdCurrent)
-          .call()
-          .then(result => {
-              console.log('result', result)
-              var cutString = result.substring(29);
-              console.log(cutString)
-              console.log(JSON.parse(atob(cutString)));
-              setDataJsonArray([...dataJsonArray, JSON.parse(atob(cutString))]);
-          })
-          .catch(err => console.log(err));
-      setLoadingArt(false);
+        /*   if (tokenIdCurrent == 0 || tokenIdCurrent) {
+             const tokenURI = await contractABI.methods
+                 .tokenURI(tokenIdCurrent)
+                 .call()
+                 .then(result => {
+                   var cutString = result.substring(29);
+                     console.log(cutString)
+                   console.log(JSON.parse(atob(cutString)));
+                   setDataJsonArray([...dataJsonArray, JSON.parse(atob(cutString))]);
+                 })
+                 .catch(err => console.log(err));
+             setLoadingArt(false);
+           } else {
+             console.log("Don't have tokenId");
+           }*/
+    };
 
- /*   if (tokenIdCurrent == 0 || tokenIdCurrent) {
-      const tokenURI = await contractABI.methods
-          .tokenURI(tokenIdCurrent)
-          .call()
-          .then(result => {
-            var cutString = result.substring(29);
-              console.log(cutString)
-            console.log(JSON.parse(atob(cutString)));
-            setDataJsonArray([...dataJsonArray, JSON.parse(atob(cutString))]);
-          })
-          .catch(err => console.log(err));
-      setLoadingArt(false);
-    } else {
-      console.log("Don't have tokenId");
-    }*/
-  };
+    const getBalance = async () => {
+        if (acc) {
+            const balance = await web3.eth.getBalance(acc);
+            const balanceInEther = web3.utils.fromWei(balance, 'ether');
+            setWalletBalance(balanceInEther);
+        } else {
+            console.log('No account');
+        }
+    };
 
-  const getBalance = async () => {
-    if (acc) {
-      const balance = await web3.eth.getBalance(acc);
-      const balanceInEther = web3.utils.fromWei(balance, 'ether');
-      setWalletBalance(balanceInEther);
-    } else {
-      console.log('No account');
+    async function renderSVG() {
+        await contractABI.methods
+            .renderFullSVGWithGrid(0)
+            .call().then(result => {
+                // var cutString = result.substring(0);
+                console.log(result);
+                // console.log(JSON.parse(atob(cutString)));
+            })
+            .catch(err => {
+                console.log(err);
+            });
     }
-  };
-
-  async function renderSVG() {
-    await contractABI.methods
-        .renderFullSVGWithGrid(0)
-        .call().then(result => {
-            // var cutString = result.substring(0);
-            console.log(result);
-            // console.log(JSON.parse(atob(cutString)));
-        })
-        .catch(err => {
-          console.log(err);
-        });
-  }
 
     async function getItem() {
         await contractABI.methods
@@ -125,107 +124,107 @@ function Home() {
             });
     }
 
-  async function addItem() {
-      /*await web3.eth.accounts.signTransaction({
-          from: acc,
-          gasPrice: "20000000000",
-          gas: "21000",
-          value: "1000000000000000000",
-            gasLimit: "53000",
-      }, PRIVATE_KEY).then(async () => {
+    async function addItem() {
+        /*await web3.eth.accounts.signTransaction({
+            from: acc,
+            gasPrice: "20000000000",
+            gas: "21000",
+            value: "1000000000000000000",
+              gasLimit: "53000",
+        }, PRIVATE_KEY).then(async () => {
 
-      });*/
+        });*/
 
-      for (let i = 0; i < DATA_INPUT.length; i++) {
-          await contractABI.methods
-              .addItem(DATA_INPUT[i].key,  DATA_INPUT[i].name,  DATA_INPUT[i].rate, DATA_INPUT[i].position)
-              .send({ from: acc }).then(result => {
-                  console.log('success', result);
-              })
-              .catch(err => {
-                  console.log(err);
-              });
-      }
+        for (let i = 0; i < DATA_INPUT.length; i++) {
+            await contractABI.methods
+                .addItem(DATA_INPUT[i].key, DATA_INPUT[i].name, DATA_INPUT[i].rate, DATA_INPUT[i].position)
+                .send({from: acc}).then(result => {
+                    console.log('success', result);
+                })
+                .catch(err => {
+                    console.log(err);
+                });
+        }
 
-  }
+    }
 
-  return (
-      <div>
-        <div className="">
-          <Space size="middle">
-            <Button size="large" type="primary" onClick={addItem}>
-              Add Item
-            </Button>
-              <Button size="large" type="primary" onClick={getItem}>
-              Get Item
-            </Button>
-              <Button size="large" type="primary" onClick={renderSVG}>
-                  Render SVG
-            </Button>
-          </Space>
+    return (
+        <div>
+            <div className="">
+                <Space size="middle">
+                    <Button size="large" type="primary" onClick={addItem}>
+                        Add Item
+                    </Button>
+                    <Button size="large" type="primary" onClick={getItem}>
+                        Get Item
+                    </Button>
+                    <Button size="large" type="primary" onClick={renderSVG}>
+                        Render SVG
+                    </Button>
+                </Space>
+            </div>
+            <Title level={4}>
+                Your Balance: {walletBalance ? walletBalance : 0} USDT
+            </Title>
+            <Space size="middle">
+                <Button size="large" onClick={() => getBalance()}>
+                    Show My Balance
+                </Button>
+            </Space>
+            <div style={{marginTop: '2%'}}>
+                <Space size="middle">
+                    <Button
+                        size="large"
+                        loading={loadingArt}
+                        onClick={async () => {
+                            setLoadingArt(true);
+                            await showArt(0);
+                        }}
+                    >
+                        TokenURI({tokenIdCurrent})
+                    </Button>
+                    <Button
+                        type="primary"
+                        size="large"
+                        loading={loadings}
+                        onClick={() => {
+                            setLoadings(true);
+                            mint();
+                        }}
+                    >
+                        Mint
+                    </Button>
+                </Space>
+            </div>
+            <Row gutter={16} style={{marginTop: '5%'}}>
+                {dataJsonArray.map((key, index) => {
+                    return (
+                        <Col span={6} key={index}>
+                            <Card
+                                hoverable
+                                style={{width: 350}}
+                                cover={<img alt="image" src={key ? key.image : ''}/>}
+                            >
+                                <Meta
+                                    title={key ? key.name : ''}
+                                    description={key ? key.description : ''}
+                                />
+                                {/*{key.attributes.map((item, index2) => {*/}
+                                {/*  return (*/}
+                                {/*      <p key={index2}>*/}
+                                {/*        <b>Name:</b> {item.Name}*/}
+                                {/*        <br />*/}
+                                {/*        <b>Size:</b> {item.size}*/}
+                                {/*      </p>*/}
+                                {/*  );*/}
+                                {/*})}*/}
+                            </Card>
+                        </Col>
+                    );
+                })}
+            </Row>
         </div>
-        <Title level={4}>
-          Your Balance: {walletBalance ? walletBalance : 0} USDT
-        </Title>
-        <Space size="middle">
-          <Button size="large" onClick={() => getBalance()}>
-            Show My Balance
-          </Button>
-        </Space>
-        <div style={{ marginTop: '2%' }}>
-          <Space size="middle">
-            <Button
-                size="large"
-                loading={loadingArt}
-                onClick={async () => {
-                  setLoadingArt(true);
-                  await showArt(0);
-                }}
-            >
-              TokenURI({tokenIdCurrent})
-            </Button>
-            <Button
-                type="primary"
-                size="large"
-                loading={loadings}
-                onClick={() => {
-                  setLoadings(true);
-                  mint();
-                }}
-            >
-              Mint
-            </Button>
-          </Space>
-        </div>
-        <Row gutter={16} style={{ marginTop: '5%' }}>
-          {dataJsonArray.map((key, index) => {
-            return (
-                <Col span={6} key={index}>
-                  <Card
-                      hoverable
-                      style={{ width: 350 }}
-                      cover={<img alt="image" src={key ? key.image : ''} />}
-                  >
-                    <Meta
-                        title={key ? key.name : ''}
-                        description={key ? key.description : ''}
-                    />
-                    {/*{key.attributes.map((item, index2) => {*/}
-                    {/*  return (*/}
-                    {/*      <p key={index2}>*/}
-                    {/*        <b>Name:</b> {item.Name}*/}
-                    {/*        <br />*/}
-                    {/*        <b>Size:</b> {item.size}*/}
-                    {/*      </p>*/}
-                    {/*  );*/}
-                    {/*})}*/}
-                  </Card>
-                </Col>
-            );
-          })}
-        </Row>
-      </div>
-  );
+    );
 }
 
 export default Home;
