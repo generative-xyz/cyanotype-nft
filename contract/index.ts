2885d3db1f595460e5830954b922d403feef7324
Max <max@bvm.network>
2024-12-12 10:31:44 +0700

    update

--- contract/index.ts
+++ contract/index.ts
@@ -2,8 +2,8 @@ const fs = require('fs').promises;
 
 type ConfigField = 'contractAddress' | 'dataContractAddress';
 let config = {
-    contractAddress: "",
-    dataContractAddress: "",
+    contractAddress: '0x1c85638e118b37167e9298c2268758e058DdfDA0',
+    dataContractAddress: '0x5FC8d32690cc91D4c39d9d3abcBD16989F875707'
 };
 
 async function initConfig() {
